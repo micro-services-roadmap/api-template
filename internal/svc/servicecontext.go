7d1d3577d887802694dc787b068ba8ce67306ffa
alice52 <zzhang_xz@163.com>
2024-12-20 12:43:30 +0800

    fix(build): refine build error

--- internal/svc/servicecontext.go
+++ internal/svc/servicecontext.go
@@ -3,7 +3,6 @@ package svc
 import (
 	"github.com/wordpress-plus/api-app/internal/config"
 	"github.com/wordpress-plus/api-app/internal/middleware"
-	"github.com/wordpress-plus/rpc-tracing/client/tracingconsoleservice"
 	"github.com/wordpress-plus/rpc-tracing/client/tracingipservice"
 	"github.com/wordpress-plus/rpc-tracing/client/tracingviewservice"
 
@@ -18,8 +17,8 @@ type ServiceContext struct {
 	CheckUrl rest.Middleware
 
 	// tracing 模块
-	TracingViewService    tracingviewservice.TracingViewService
-	TracingIPService      tracingipservice.TracingIPService
+	TracingViewService tracingviewservice.TracingViewService
+	TracingIPService   tracingipservice.TracingIPService
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
@@ -29,7 +28,7 @@ func NewServiceContext(c config.Config) *ServiceContext {
 		Config:   c,
 		CheckUrl: middleware.NewCheckUrlMiddleware().Handle,
 
-		TracingViewService:    tracingviewservice.NewTracingViewService(trackingClient),
-		TracingIPService:      tracingipservice.NewTracingIPService(trackingClient),
+		TracingViewService: tracingviewservice.NewTracingViewService(trackingClient),
+		TracingIPService:   tracingipservice.NewTracingIPService(trackingClient),
 	}
 }
